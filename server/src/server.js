bc18cf19b6b6dd16507c0b821d10746687db95e0
911-duck <zhenyabiruk800@gmail.com>
2026-05-31 12:11:49 +0300

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000
 app.use(
 	cors({
 		methods: 'GET, POST, DELETE, PUT',
-		origin: 'http://localhost:5173',
+		origin: 'https://pr-production-2481.up.railway.app',
 		credentials: true
 	})
 )
