6fd52f39c68572eccd69a0828119201e5c8864ad
911-duck <zhenyabiruk800@gmail.com>
2026-06-01 23:30:59 +0300

    Update Image.jsx

--- client/src/components/Image/Image.jsx
+++ client/src/components/Image/Image.jsx
@@ -6,19 +6,19 @@ function Image({ url, heigth, width, color, size, hover}) {
     if(hover == 1) {
         return (
             <>
-            <div className={styles.imageH} style={{backgroundColor: color, backgroundImage: `url("${url}")`, width: width, height: heigth, backgroundSize: `${size}%`}} ></div>
+            <div className={styles.imageH} style={{backgroundColor: color, backgroundImage: `url(${url})`, width: width, height: heigth, backgroundSize: `${size}%`}} ></div>
             </>
         )
     }else if(hover == 2){
         return (
             <>
-            <div className={styles.imageW} style={{backgroundColor: color, backgroundImage: `url("${url}")`, width: width, height: heigth, backgroundSize: `${size}%`}} ></div>
+            <div className={styles.imageW} style={{backgroundColor: color, backgroundImage: `url(${url})`, width: width, height: heigth, backgroundSize: `${size}%`}} ></div>
             </>
         )
     }
     return (
             <>
-            <div className={styles.image} style={{backgroundColor: color, backgroundImage: `url("${url}")`, width: width, height: heigth, backgroundSize: `${size}%`}} ></div>
+            <div className={styles.image} style={{backgroundColor: color, backgroundImage: `url(${url})`, width: width, height: heigth, backgroundSize: `${size}%`}} ></div>
             </>
         )
 }
